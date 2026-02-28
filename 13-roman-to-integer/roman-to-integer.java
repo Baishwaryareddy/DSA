61b7c11d8fce211e2d9332fadf8315c5d4e0dab3
BADUGULA AISHWARYA REDDY <161433891+Baishwaryareddy@users.noreply.github.com>
2026-02-28 22:21:43 +0530

    Time: 3 ms (77.14%) | Memory: 46.2 MB (94.48%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -3,8 +3,7 @@ class Solution {
         int sum=0;
         int start=0;
        while(start<s.length()){
-        if(start + 1 < s.length() &&
-               check(s.charAt(start + 1)) > check(s.charAt(start))) {
+        if(start + 1 < s.length() &&check(s.charAt(start + 1)) > check(s.charAt(start))) {
                 sum += check(s.charAt(start + 1)) - check(s.charAt(start));
                 start += 2; 
             }
@@ -12,30 +11,14 @@ class Solution {
             sum+=check(s.charAt(start));
             start++;
         }
-       }
+        }
        return sum; 
     }
     int check(char ch){
-        if(ch=='I'){
-            return 1;
-        }
-        else if(ch=='V'){
-            return 5;
-        }
-        else if(ch=='X'){
-            return 10;
-        }
-        else if(ch=='L'){
-            return 50;
-        }
-        else if(ch=='C'){
-            return 100;
-        }
-        else if(ch=='D'){
-            return 500;
-        }
-        else {
-            return 1000;
-        }
-    }
-}
+        if(ch=='I'){ return 1;}
+        else if(ch=='V'){ return 5;}
+        else if(ch=='X'){return 10;}
+        else if(ch=='L'){return 50;}
+        else if(ch=='C'){return 100;}
+        else if(ch=='D'){return 500;}
+        else {return 1000;}}}
